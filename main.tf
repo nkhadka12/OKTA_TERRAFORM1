02a7738c09f1451cc164b9f480f8703ee3295580
Nischal Khadka <82045084+nkhadka12@users.noreply.github.com>
2023-09-18 12:29:41 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,31 @@
+terraform { 
+   cloud {
+    organization = "TerraForm_Workshop12"
+    workspaces {
+      name = "Okta_Terraform"
+    }
+  }
+   
+  required_providers {
+    okta = {
+      source = "okta/okta"
+
+    }
+
+
+  }
+}
+provider "okta"{
+    org_name = "dev-10812830"
+    base_url = "okta.com"
+    client_id = "0oab9bdzjkWEzsqkR5d7"
+    scopes = ["okta.groups.manage"]
+    private_key = "${path.module}/rsa.pem"
+
+}
+
+
+resource "okta_group" "terra_group"{
+  name = "Terra_Form_Group"
+}
 
